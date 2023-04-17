f251f73673b7f74387c94caa1e71d1b9b86d33fd
Aaiory <alexis.hernandez@ufl.edu>
2023-04-16 22:14:48 -0400

    Revert "Upload 3"
    This reverts commit 27f866e694dc4e8e5b188f17d1829e35752cb4c5.

--- RBTree.cpp
+++ RBTree.cpp
@@ -227,41 +227,31 @@ RBTree::Node *RBTree::findNode(Node* x, const std::string& category) {
     }
 }
 
-std::vector<Food> RBTree::search(const std::string& category) {
-    std::vector<Food> results;
-    searchHelper(category, root, results);
-    return results;
-}
-
-void RBTree::searchHelper(const std::string& category, Node* x, std::vector<Food>& results) {
-    if (x == nullptr) {
+void RBTree::remove(const std::string& category) {
+    Node* z = searchHelper(category, root);
+    if (z == nullptr) {
         return;
     }
-    if (category < x->data.getCategory()) {
-        searchHelper(category, x->left, results);
-    } else if (category > x->data.getCategory()) {
-        searchHelper(category, x->right, results);
-    } else {
-        // We found a node with the matching category key, so add its food item to the results vector
-        results.push_back(x->data);
-        // Recursively search the left and right subtrees for additional matching nodes
-        searchHelper(category, x->left, results);
-        searchHelper(category, x->right, results);
-    }
+    deleteNode(z);
 }
 
-
-void RBTree::uniqueKeysHelper(RBTree::Node *x, std::set<std::string> &keys) {
-    if (x == nullptr) {
-        return;
+Food* RBTree::search(const std::string& category) {
+    Node* z = searchHelper(category, root);
+    if (z == nullptr) {
+        return nullptr;
     }
-    uniqueKeysHelper(x->left, keys);
-    keys.insert(x->data.getCategory());
-    uniqueKeysHelper(x->right, keys);
+    return &(z->data);
 }
 
-std::set<std::string> RBTree::uniqueKeys() {
-    std::set<std::string> keys;
-    uniqueKeysHelper(root, keys);
-    return keys;
+RBTree::Node* RBTree::searchHelper(const std::string& category, Node* x) {
+    while (x != nullptr) {
+        if (category < x->data.getCategory()) {
+            x = x->left;
+        } else if (category > x->data.getCategory()) {
+            x = x->right;
+        } else {
+            return x;
+        }
+    }
+    return nullptr;
 }
--- RBTree.h
+++ RBTree.h
@@ -2,7 +2,6 @@
 #define FINAL_PROJECT_2_RBTREE_H
 
 #include <utility>
-#include <set>
 #include "Food.h"
 
 class RBTree {
@@ -29,15 +28,14 @@ private:
     static Node* minimum(Node* x);
     static Node* maximum(Node* x);
     Node* findNode(Node* x, const std::string& category);
-    static void uniqueKeysHelper(Node* x, std::set<std::string>& keys);
+    static Node *searchHelper(const std::string &category, Node *x);
 
 public:
     RBTree() : root(nullptr) {}
     ~RBTree() { delete root; }
     void insert(Food food);
-    std::set<std::string> uniqueKeys();
-    std::vector<Food> search(const std::string &category);
-    void searchHelper(const std::string &category, Node *x, std::vector<Food> &results);
+    void remove(const std::string& category);
+    Food* search(const std::string& category);
 };
 
 #endif //FINAL_PROJECT_2_RBTREE_H
